10a3e6946864c75e38277dfa6fa72bdc9298bcb7
Alexander <alexxx547@gmail.com>
2020-03-11 18:44:11 +0300

    Updates

---
+++ precache-manifest.fada708f467f7f2081027d40d359d17e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bc6543c1f28bbdb83799fffb2edba8",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "5df8d0cbd4055b27b4d6",
+    "url": "/aviasales/static/css/main.764d14c7.chunk.css"
+  },
+  {
+    "revision": "b230ddaf08251a5f68cf",
+    "url": "/aviasales/static/js/2.46962679.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/aviasales/static/js/2.46962679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df8d0cbd4055b27b4d6",
+    "url": "/aviasales/static/js/main.254d0fe3.chunk.js"
+  },
+  {
+    "revision": "3711b0f9c4416dd277db",
+    "url": "/aviasales/static/js/runtime-main.d5d7c77d.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/aviasales/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/aviasales/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales/static/media/check.68ca57dc.svg"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales/static/media/logo.0c089234.svg"
+  }
+]);
